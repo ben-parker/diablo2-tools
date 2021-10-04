e79306f5ec18d45ddacd5a2ffe0f9c0c2255f178
Ben Parker <bepwasd@gmail.com>
2021-10-04 14:03:31 -0600

    fix base path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const JSONMinifyPlugin = require('node-json-minify');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    publicPath: '/diablo2-tools/',
     configureWebpack: {
         plugins: [
             new CopyWebpackPlugin({
